5335a04ac6de35b9684e67cb2aef5809477ad4eb
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-02-27 16:43:52 -0300

    Question 03

---
+++ src/question03.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class question03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite o valor de A: ");
+        int a = sc.nextInt();
+        System.out.print("Digite o valor de B: ");
+        int b = sc.nextInt();
+
+        if (a % b == 0 || b % a == 0) {
+            System.out.println("São múltiplos.");
+        }
+        else {
+            System.out.println("Não são múltiplos.");
+        }
+
+        sc.close();
+    }
+}
