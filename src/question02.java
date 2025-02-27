38a59a84e1350db9d113d6f50dadea86d1a0478a
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-02-27 16:39:59 -0300

    Question 02

---
+++ src/question02.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class question02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("Par.");
+        }
+        else {
+            System.out.println("Ímpar.");
+        }
+
+        sc.close();
+    }
+}
