81f31ee132b0d8bf46af5a0611053cb3bd1bcc9f
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-02-28 14:42:16 -0300

    Optional syntax

---
+++ SintaxeOpcional.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Locale; 
+
+public class SintaxeOpcional {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Quanto minutos você consumiu da franquia?: ");
+        int minutos = sc.nextInt();
+
+        double conta = 50.0;
+        if (minutos > 100) {
+            conta = conta + (minutos - 100) * 2.0;
+        }
+
+        System.out.printf("Valor da conta: %.2f%n", conta);
+
+        sc.close();
+    }
+}
