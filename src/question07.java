6e8eb964d46e499bc14298431baf5d67110cb178
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-02-28 14:09:28 -0300

    Question 07

---
+++ src/question07.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class question07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite o valor de X: ");
+        int x = sc.nextInt();
+        System.out.print("Digite o valor de Y: ");
+        int y = sc.nextInt();
+
+        if (x == 0 || y == 0) {
+            System.out.println("Origem!");
+        }
+        else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        }
+        else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        }
+        else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        }
+        else {
+            System.out.println("Q4");
+        }
+
+        sc.close();
+    }
+}
