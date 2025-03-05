da5f2de7acbfb8df910ea8147d5baf46afea4073
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-03-04 21:23:59 -0300

    If-else conditional structure review 01

---
+++ revisoes/Condicional01.java
@@ -0,0 +1,23 @@
+package revisoes;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Condicional01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite um valor inteiro: ");
+        int valor = sc.nextInt();
+
+        if (valor > 0) {
+            System.out.println("Número positivo!");
+        }
+        else {
+            System.out.println("Negativo!");
+        }
+
+        sc.close();
+    }
+}
